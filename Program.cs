78f71e59a66260741c74499ec65d1d93d32cb3ac
stinemas <63400786+stinemas@users.noreply.github.com>
2021-09-03 14:46:56 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CarSpeedLimit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car(0, 50);
+
+            //car.CheckSpeed();
+
+            var ui = new Ui(car);
+        }
+    }
+}
